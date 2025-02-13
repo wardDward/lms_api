26343fa906cab89fd310d04417b594bb8cf40250
wardDward <edward.taligatos03@gmail.com>
2025-02-13 12:52:49 +0800

    user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Role;
+use Hash;
 use Illuminate\Http\Request;
 use App\Models\User;
 
@@ -11,23 +13,26 @@ class UserController extends Controller
     public function register(Request $request)
     {
         $data = $request->validate([
-            'firsname' => 'required|max:255',
+            'firstname' => 'required|max:255',
             'middlename' => 'nullable|max:255',
             'lastname' => 'required|max:255',
             'password' => 'required|confirmed|max:50',
             'email' => 'required|max:255|unique:users,email',
-            "gender" => 'required|in:male,female'
+            "gender" => 'required|in:male,female',
+            "role" => 'required|exists:role,name'
         ]);
 
-        $avatar = $request->avatar === 'male' ? 'male_dp.png' : 'woman_dp.png';
+        $role = Role::where('name', $data['role'])->first();
+        $avatar = $data['gender'] === 'male' ? 'male_dp.png' : 'woman_dp.png';
 
         $user = User::create([
-            'firstname' => $data->firstname,
-            'middlename' => $data->middlename,
-            'lastname' => $data->lastname,
-            'password' => $data->password,
-            'email' => $data->email,
-            'gender' => $data->gender,
+            'firstname' => $data['firstname'],
+            'middlename' => $data['middlename'],
+            'lastname' => $data['lastname'],
+            'password' => Hash::make($data['password']),
+            'email' => $data['email'],
+            'gender' => $data['gender'],
+            'role' => $role,
             'avatar' => $avatar
         ]);
 
